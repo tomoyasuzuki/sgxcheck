04ffa89b09991733f274042ce617d8736252edfd
tomoyasuzuki <tomoya.s0121@icloud.com>
2021-09-21 14:43:14 +0900

    Add debug mode

--- App/App.c
+++ App/App.c
@@ -1,6 +1,8 @@
 #include "Enclave_u.h"
 #include <stdio.h>
 #include <stdint.h>
+#include <string.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/types.h>
@@ -8,6 +10,7 @@
 #include <sgx_urts.h>
 #include <dirent.h>
 #include "utils.h"
+#include "sgx_tseal.h"
 
 #define MAX_FILE_NUMS 5
 #define MAX_FILE_SIZE 104 * 1024 * 500
@@ -17,24 +20,17 @@ sgx_enclave_id_t eid = 100;
 int file_nums = 0;
 char *dir_path = "/usr/bin/";
 char *type = "-c";
-char target_files[MAX_FILE_NUMS][200] = {"/usr/bin/ls", 
-                                         "/usr/bin/ps", 
-                                         "/usr/bin/apt", 
-                                         "/usr/bin/x86_64-linux-gnu-strip", 
-                                         "/usr/bin/utmpdump"};
+int debug = 0;
+char target_files[MAX_FILE_NUMS][200] = { "/usr/bin/ls", 
+                                          "/usr/bin/ps", 
+                                          "/usr/bin/apt", 
+                                          "/usr/bin/x86_64-linux-gnu-strip", 
+                                          "/usr/bin/utmpdump" };
 
 // ======= utils ==============
 
 void o_printf(char *str, int size) {
     printf(str);
-}
-
-void o_printf_d(char *str, int d) {
-    printf("out: %s %d\n", str, d);
-}
-
-void o_strcat(char *dst, size_t d_size, char *src, size_t s_size) {
-    strcat(dst, src);
 }
 
 // ======= enclave ops ========
@@ -90,11 +86,14 @@ void  get_all_files(char *path) {
 }
 
 void get_file_hash(int i) {
-    FILE *fp;
+    FILE *fp, *sealed_hash_fp;
     struct stat st;
     void *buf;
     uint8_t hash[32];
     uint8_t old_hash[32];
+    uint32_t sealed_data_size;
+    uint8_t *sealed_buf;
+    int seal_err;
     sgx_status_t err;
     char *path = target_files[i];
 
@@ -110,18 +109,71 @@ void get_file_hash(int i) {
 
     fread(buf, st.st_size, 1, fp);
 
-    err = calc_hash(eid, path, buf, st.st_size, hash);
+    if (debug) {
+        if ((err = calc_hash_debug(eid, path, buf, st.st_size, hash)) != SGX_SUCCESS) {
+            print_with_color(path, "red");
+            printf("Failed to calculate hash value, code=%x\n", err);
+            return;
+        }
+    } else {
+        if ((err = calc_hash(eid, path, buf, st.st_size)) != SGX_SUCCESS) {
+            print_with_color(path, "red");
+            printf("Failed to calculate hash value, code=%x\n", err);
+            return;
+        }
+    }
+    
 
     if (err == SGX_SUCCESS) {
+        if (debug)
+            print_with_color("[DEBUG]", "green");
+        
         print_with_color(path, "green");
-        dump_hash(hash);
+
+        if (debug) {
+            dump_hash(hash);
+        } else {
+            printf("Successed to calculate hash.\n");
+        }
     } else {
         print_with_color(path, "red");
         printf("Failed to get hash, error code=%x\n", err);
     }
 
+    if ((err = get_sealed_data_size(eid, &sealed_data_size, path)) != SGX_SUCCESS) {
+        print_with_color(path, "red");
+        printf("Failed to get sealed data size, code=%x\n", err);
+    }
+
+    sealed_buf = malloc(sealed_data_size);
+
+    if ((err = seal_data(eid, &seal_err, path, sealed_buf, sealed_data_size)) != SGX_SUCCESS) {
+        print_with_color(path, "red");
+        printf("Failed to get sealed data size, code=%x\n", err);
+    }
+
+    int index = 0;
+    char hash_path[100];
+
+    err = get_index(eid, &index, path);
+
+    snprintf(hash_path, 100, "%d", index);
+
+    if ((sealed_hash_fp = fopen(hash_path, "w")) == NULL) {
+        print_with_color(path, "red");
+        printf("Failed to create sealed_hash_file\n");
+    }
+
+    size_t wsize = fwrite(sealed_buf, sealed_data_size, 1, sealed_hash_fp);
+    if (wsize < 1) {
+        print_with_color(path, "red");
+        printf("Failed to write sealed_hash\n");
+    }
+
     fclose(fp);
     free(buf);
+
+    return;
 }
 
 void get_all_file_hashes() {
@@ -132,14 +184,75 @@ void get_all_file_hashes() {
     }
 }
 
-
 void check_hash(int i) {
+    sgx_status_t err;
+    FILE *fp, *hash_fp;
+    struct stat fpst,hash_fpst;
+    char *path = target_files[i];
+    int index;
+    char hash_path[100];
+    void *file_buf;
+    uint8_t new_hash[HASH_SIZE];
+    uint8_t *sealed_hash;
+    uint32_t sealed_data_size;
+    int cmp_err;
+
+    if ((fp = fopen(path, "r")) == NULL) {
+        print_with_color(path, "red");
+        printf("Failed to open file\n");
+        return;
+    }
+
+    err = get_index(eid, &index, path);
+    snprintf(hash_path, 100,"%d", index);
+
+    if ((hash_fp = fopen(hash_path, "r")) == NULL) {
+        print_with_color(hash_path, "red");
+        printf("Failed to open file\n");
+        return;
+    }
+
+    stat(path, &fpst);
+    stat(hash_path, &hash_fpst);
+    
+    if (fpst.st_size > MAX_FILE_SIZE) {
+        printf("%s: File size is too big ...\n", path); 
+        fclose(fp);
+        return;
+    }
+    
+    file_buf = malloc(fpst.st_size);
+    sealed_hash = malloc(hash_fpst.st_size);
+
+    fread(file_buf, fpst.st_size, 1, fp);
+    fread(sealed_hash, hash_fpst.st_size, 1, hash_fp);
+
+    err = calc_hash(eid, path, file_buf, fpst.st_size);
+    err = cmp_hash(eid, &cmp_err, path, sealed_hash, (uint32_t)hash_fpst.st_size);
+
+    if (cmp_err) {
+        print_with_color(path, "green");
+        printf("File not broken.\n");
+    } else {
+        print_with_color(path, "red");
+        printf("File broken!!\n");
+    }
+
+    free(file_buf);
+    fclose(fp);
+    fclose(hash_fp);
+
+    return;
+}
+
+
+void check_hash_debug(int i) {
     sgx_status_t err;
     FILE *fp;
     struct stat st;
     void *file_buf;
-    uint8_t new_hash[32];
-    uint8_t old_hash[32];
+    uint8_t new_hash[HASH_SIZE];
+    uint8_t old_hash[HASH_SIZE];
     char *path = target_files[i];
 
     if ((fp = fopen(path, "r")) == NULL) {
@@ -160,14 +273,18 @@ void check_hash(int i) {
 
     fread((void*)file_buf, (uint32_t)st.st_size, 1, fp);
 
-    err = calc_hash(eid, path, file_buf, st.st_size, new_hash);
+    err = calc_hash_debug(eid, path, file_buf, st.st_size, new_hash);
     err = get_hash(eid, path, old_hash);
 
     if (err == SGX_SUCCESS) {
         if (!memcmp(new_hash, old_hash, HASH_SIZE)) {
+            memset(old_hash, 0, HASH_SIZE);
+            memset(new_hash, 0, HASH_SIZE);
             print_with_color(path, "green");
             printf("file not broken\n");
         } else {
+            memset(old_hash, 0, HASH_SIZE);
+            memset(new_hash, 0, HASH_SIZE);
             print_with_color(path, "red");
             printf("file broken!!!\n");
             printf("Hash_new: ");
@@ -188,19 +305,30 @@ void check_all_file_hash() {
     printf("========== Checking hash value ==========\n");
 
     for (int i = 0; i < 5; i++) {
-        check_hash(i);
+        if (debug) {
+            check_hash_debug(i);
+        } else {
+            check_hash(i);
+        }
     }
 }
 
+int check_param(char *param) {
+    return (strcmp(param, "-i") && strcmp(param, "-c"));
+}
 
 int main(int argc, char **argv) {
     sgx_status_t err;
-
     if (argc > 1) {
-        if (strcmp(argv[1], "-i") && strcmp(argv[1], "-c")) {
+        if (check_param(argv[1])) {
             exit(1);
         } else {
             type = argv[1];
+            if (argv[2]) {
+                if (!strcmp(argv[2], "-d")) {
+                    debug = 1;
+                }
+            }
         }
     }
 
@@ -214,7 +342,11 @@ int main(int argc, char **argv) {
 
     if (!strcmp(type, "-i")) {
         get_all_file_hashes();
-    } else {
+    } else if (!strcmp(type, "-c")) {
         check_all_file_hash();
+    } else {
+        perror("Paramater Invalid");
     }
+
+    return 0;
 }
--- Enclave/Enclave.c
+++ Enclave/Enclave.c
@@ -3,10 +3,12 @@
 #include "stdio.h"
 #include "sgx_tcrypto.h"
 #include "stdlib.h"
+#include "sgx_tseal.h"
 
 #define TABLE_SIZE 100
 #define HASH_SIZE 32
 #define MAX_PATH_LENGTH 200
+#define MAC "MAC"
 
 struct hash_data {
     uint8_t hash[32];
@@ -14,8 +16,15 @@ struct hash_data {
 
 struct hash_data *hash_list;
 
+int unseal_data(uint8_t *sealed_data, uint32_t data_size, uint8_t *unsealed_hash);
+int get_hash_index(char *str, int table_size);
+
 void untrusted_printf(char *str) {
     o_printf(str, strlen(str));
+}
+
+int get_index(char *path) {
+    return get_hash_index(path, TABLE_SIZE);
 }
 
 // Generate hash_list index
@@ -31,17 +40,53 @@ int get_hash_index(char *str, int table_size) {
     return sum % table_size;
 }
 
+// Check hash value
 void init_hash_list() {
     hash_list = malloc(sizeof(struct hash_data) * TABLE_SIZE);
 }
 
-int cmp_hash(uint8_t *old, uint8_t *new, size_t size) {
-    return memcmp((void*)old, (void*)new, size);
+int cmp_hash(char *path, uint8_t *sealed_hash, size_t sh_size) {
+    uint8_t new_hash[HASH_SIZE];
+    uint8_t old_hash[HASH_SIZE];
+
+    memcpy(new_hash, hash_list[get_index(path)].hash, HASH_SIZE);
+
+    if (!unseal_data(sealed_hash, sh_size, old_hash)) {
+        untrusted_printf("Failed to unseal hash\n");
+        return 0;
+    }
+
+    return !memcmp(new_hash, old_hash, HASH_SIZE) ? 1 : 0; 
 }
 
-// Calculate SHA256 hash value
-void calc_hash(char *path, void *buf, size_t buf_size, uint8_t *dst) {
-    sgx_sha_state_handle_t state = malloc(1024 * 1024);
+// Check hash value
+int cmp_hash_debug(char *path, uint8_t *new_hash, uint8_t *sealed_hash, uint32_t sh_size) {
+    uint8_t *old_hash = malloc(HASH_SIZE);
+
+    if (!unseal_data(sealed_hash, sh_size, old_hash)) {
+        untrusted_printf("Failed to unseal hash\n");
+        return 0;
+    }
+
+    return  !memcmp(new_hash, old_hash, HASH_SIZE) ? 1 : 0;
+}
+
+void calc_hash(char *path, void *buf, size_t buf_size) {
+    sgx_sha_state_handle_t state = malloc(sizeof(sgx_sha_state_handle_t));
+    sgx_sha256_hash_t hash;
+    sgx_status_t err;
+    
+    sgx_sha256_init(state);
+
+    err = sgx_sha256_msg(buf, buf_size, &hash);
+
+    if (err == SGX_SUCCESS) {
+        memcpy(hash_list[get_index(path)].hash, hash, HASH_SIZE);
+    } 
+}
+
+void calc_hash_debug(char *path, void *buf, size_t buf_size, uint8_t *dst) {
+    sgx_sha_state_handle_t state = malloc(sizeof(sgx_sha_state_handle_t));
     sgx_sha256_hash_t hash;
     sgx_status_t err;
     
@@ -51,16 +96,62 @@ void calc_hash(char *path, void *buf, size_t buf_size, uint8_t *dst) {
 
     if (err == SGX_SUCCESS) {
         memcpy(dst, hash, HASH_SIZE);
+        memcpy(hash_list[get_index(path)].hash, hash, HASH_SIZE);
     } else {
-        o_printf_d("code", err);
+        untrusted_printf("Failed to calculate hash\n");
     }
-
-    int index = get_hash_index(path, TABLE_SIZE);
-    
-    memcpy(hash_list[index].hash, hash, HASH_SIZE);
 }
 
 void get_hash(char *path, uint8_t *hash) {
     int index = get_hash_index(path, TABLE_SIZE);
     memcpy(hash, hash_list[index].hash, HASH_SIZE);
+}
+
+uint32_t get_sealed_data_size(char *path) {
+    int index = get_hash_index(path, TABLE_SIZE);
+
+    return sgx_calc_sealed_data_size((uint32_t)sizeof(MAC), (uint32_t)sizeof(hash_list[index].hash));
+}
+
+int seal_data(char *path, uint8_t *dst_buf, uint32_t size) {
+    int index = get_hash_index(path, TABLE_SIZE);
+    uint8_t *tmp_buf;
+    uint32_t mac_size = (uint32_t)sizeof(MAC);
+    uint32_t secret_size = HASH_SIZE;
+    uint32_t data_size = sgx_calc_sealed_data_size(mac_size, secret_size);
+    sgx_status_t err;
+
+    tmp_buf = (uint8_t *)malloc(data_size);
+
+    if ((err = sgx_seal_data(mac_size, 
+                            (const uint8_t *)MAC, 
+                            secret_size, 
+                            (const uint8_t *)hash_list[index].hash, 
+                            data_size, (sgx_sealed_data_t *)tmp_buf)) == SGX_SUCCESS) {
+        
+        memcpy(dst_buf, tmp_buf, data_size);
+        free(tmp_buf);
+        return 1;
+    }
+
+    free(tmp_buf);
+    return 0;
+}
+
+int unseal_data(uint8_t *sealed_data, uint32_t data_size, uint8_t *unsealed_hash) {
+    sgx_status_t err;
+    uint32_t mac_len = sgx_get_add_mac_txt_len((const sgx_sealed_data_t *)sealed_data);
+    uint32_t decrypt_data_len= sgx_get_encrypt_txt_len((const sgx_sealed_data_t *)sealed_data);
+
+    uint8_t *de_mac_text =(uint8_t *)malloc(mac_len);
+
+    if ((err = sgx_unseal_data((const sgx_sealed_data_t *)sealed_data,
+                                de_mac_text, 
+                                &mac_len, 
+                                unsealed_hash, 
+                                &decrypt_data_len)) == SGX_SUCCESS) {
+       return 1;
+    } 
+
+    return 0;
 }
